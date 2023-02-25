3837f1dccfdc8a5e3b5ad0c65e562198bc3491b1
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-02-25 01:43:39 +0100

    Update config.lua

---
+++ shared/config.lua
@@ -0,0 +1,190 @@
+Config = Config or {}
+
+Config.Admin = {
+    ["admincar"] = {
+        type = "server", -- "client", "server", "function", "command"
+        event = "ps-adminmenu:server:ReviveAll", 
+        perms = {"god", "admin", "mod"}, -- Permissions
+        show = true 
+    },
+    ["banplayer"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["bringplayer"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["changeweather"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["clearinventory"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["clothingmenu"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["customs"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["fixvehicle"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["freeze"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["givecash"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["givenuifocus"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["god"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["kick"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["kickall"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["kill"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["maxammo"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["muteplayer"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["noclip"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["openinventory"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["revive"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["reviveall"] = {
+        type = "server",
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["reviveradius"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["setjob"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["spawncar"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["spectate"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["teleport"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["teleportcoords"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["teleportmarker"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["unbanplayer"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["uncuff"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    },
+    ["warn"] = {
+        type = "server", 
+        event = "ps-adminmenu:server:ReviveAll",
+        perms = {"god", "admin", "mod"}, 
+        show = true
+    }
+}
