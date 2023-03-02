98dd6a3471d02f18c7878bfb96030b85a1110b63
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-03-02 20:05:05 +0100

    New system for buttons

--- ui/src/components/Wizard.svelte
+++ ui/src/components/Wizard.svelte
@@ -3,6 +3,7 @@
   import { fetchNui } from '@utils/fetchNui'
   import { slide } from 'svelte/transition'
 	import Buttons from '@components/Buttons.svelte'
+  import ButtonsDropdown  from '@components/ButtonsDropdown.svelte'
 
 
   let searchTerm = "";
@@ -139,76 +140,39 @@ function banPlayer() {
   <div class="buttons-container">
     
     <Buttons buttonText={'Noclip'} fetchFunction={'ToggleNoClip'} id="noclip"/>
-    <Buttons buttonText={'Admin'} fetchFunction={'ToggleAdmin'} id="admin"/>
-    <Buttons buttonText={'Kickall'} fetchFunction={'ToggleKickall'} id="kickall"/>
     
-        {#if (searchTerm === '' || 'Ban Player'.toLowerCase().includes(searchTerm)) && (!showFavorites || selectedBanplayer)}
-        <button class="menu-button" on:click={toggleDropdownBanplayer}>
-          <i class={`far fa-star ${selectedBanplayer ? 'fas' : 'far'}`} on:click={toggleSelectionBanplayer} style="margin-right: 10px; color: {selectedBanplayer ? 'var(--starcolor)' : 'var(--textcolor)'}"></i> {'Ban Player'} 
-          <i class="fas fa-angle-right dropdown-icon" style="transform: rotate({showDropdownBanplayer ? 90 : 0}deg);"></i>
-        </button>
-              
-        {#if showDropdownBanplayer}
-        <div class="dropdown" style="padding-bottom: 12.4rem" transition:slide="{{duration: 300}}">
-          <div class="dropdown-buttons-container">
-            <input placeholder="ID" class="dropdown-inputs">
-            <input placeholder="Reason" class="dropdown-inputs">
-            <input placeholder="Length" class="dropdown-inputs">
-            <button class="dropdown-buttons" on:click={banPlayer} >Ban</button>
-          </div>
-        </div>
-        {/if}
-      {/if}
+    <ButtonsDropdown buttonText={'Admincar'} fetchFunction={'ToggleAdmincar'} id="admincar">
+      <div class="dropdown-buttons-container">
+        <input class="dropdown-inputs" placeholder="Coords">
+       <button class="dropdown-buttons" on:click={() => console.log('Teleport button clicked')}>Teleport</button>
 
-        {#if (searchTerm === '' || 'Bring Player'.toLowerCase().includes(searchTerm)) && (!showFavorites || selectedBringplayer)}
-        <button class="menu-button" on:click={toggleDropdownBringplayer}>
-          <i class={`far fa-star ${selectedBringplayer ? 'fas' : 'far'}`} on:click={toggleSelectionBringplayer} style="margin-right: 10px; color: {selectedBringplayer ? 'var(--starcolor)' : 'var(--textcolor)'}"></i> {'Bring Player'} 
-          <i class="fas fa-angle-right dropdown-icon" style="transform: rotate({showDropdownBringplayer ? 90 : 0}deg);"></i>
-        </button>
-      
-        {#if showDropdownBringplayer}
-        <div class="dropdown" transition:slide="{{duration: 300}}">
-          <div class="dropdown-buttons-container">
-            <input class="dropdown-inputs" placeholder="ID">
-            <button class="dropdown-buttons">Submit</button>
-          </div>
-        </div>
-        {/if}
-      {/if}
-
-      {#if (searchTerm === '' || 'Kick'.toLowerCase().includes(searchTerm)) && (!showFavorites || selectedKickplayer)}
-      <button class="menu-button" on:click={toggleDropdownKickplayer}>
-        <i class={`far fa-star ${selectedKickplayer ? 'fas' : 'far'}`} on:click={toggleSelectionKickplayer} style="margin-right: 10px; color: {selectedKickplayer ? 'var(--starcolor)' : 'var(--textcolor)'}"></i> {'Kick'} 
-        <i class="fas fa-angle-right dropdown-icon" style="transform: rotate({showDropdownKickplayer ? 90 : 0}deg);"></i>
-      </button>
-            
-      {#if showDropdownKickplayer}
-      <div class="dropdown" style="padding-bottom: 8.7rem" transition:slide="{{duration: 300}}">
-        <div class="dropdown-buttons-container">
-          <input placeholder="ID" class="dropdown-inputs">
-          <input placeholder="Reason" class="dropdown-inputs">
-          <button class="dropdown-buttons" on:click={banPlayer} >Kick</button>
-        </div>
       </div>
-      {/if}
-    {/if}
+    </ButtonsDropdown>
 
-      {#if (searchTerm === '' || 'Teleport Coords'.toLowerCase().includes(searchTerm)) && (!showFavorites || selectedTeleportCoords)}
-      <button class="menu-button" on:click={toggleDropdownTeleportCoords}>
-        <i class={`far fa-star ${selectedTeleportCoords ? 'fas' : 'far'}`} on:click={toggleSelectionTeleportCoords} style="margin-right: 10px; color: {selectedTeleportCoords ? 'var(--starcolor)' : 'var(--textcolor)'}"></i> {'Teleport Coords'} 
-        <i class="fas fa-angle-right dropdown-icon" style="transform: rotate({showDropdownTeleportCoords ? 90 : 0}deg);"></i>
-      </button>
-            
-      {#if showDropdownTeleportCoords}
-      <div class="dropdown" transition:slide="{{duration: 300}}">
-        <div class="dropdown-buttons-container">
-          <input class="dropdown-inputs" placeholder="Coords">
-          <button class="dropdown-buttons" on:click={banPlayer}>Teleport</button>
-        </div>
+    <ButtonsDropdown buttonText={'Admincar'} fetchFunction={'ToggleAdmincar'} id="admincar">
+      <div class="dropdown-buttons-container">
+        <input class="dropdown-inputs" placeholder="Coords">
+        <input class="dropdown-inputs" placeholder="Coords">
+        <input class="dropdown-inputs" placeholder="Coords">
+       <button class="dropdown-buttons" on:click={() => console.log('Teleport button clicked')}>Teleport</button>
       </div>
-      {/if}
-    {/if}
+    </ButtonsDropdown>
 
+    <ButtonsDropdown buttonText={'Admincar'} fetchFunction={'ToggleAdmincar'} id="admincar">
+      <div class="dropdown-buttons-container">
+        <input class="dropdown-inputs" placeholder="Coords">
+        <input class="dropdown-inputs" placeholder="Coords">
+        <input class="dropdown-inputs" placeholder="Coords">
+        <input class="dropdown-inputs" placeholder="Coords">
+       <button class="dropdown-buttons" on:click={() => console.log('Teleport button clicked')}>Teleport</button>
+       <button class="dropdown-buttons" on:click={() => console.log('Teleport button clicked')}>Teleport</button>
+       <button class="dropdown-buttons" on:click={() => console.log('Teleport button clicked')}>Teleport</button>
+       <button class="dropdown-buttons" on:click={() => console.log('Teleport button clicked')}>Teleport</button>
+      </div>
+    </ButtonsDropdown>
+
+    
+ 
   </div>
 </div>
 
@@ -244,20 +208,6 @@ function banPlayer() {
   margin-top: 1rem;
 }
 
-.menu-button {
-  padding: 0.9rem;
-  width: 100%;
-  background-color: var(--color-3);
-  color: var(--textcolor);
-  font-family: 'Roboto', sans-serif;
-  margin-top: 0.5rem;
-  cursor: pointer;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  text-align: left;
-}
-
 .button-all {
   border: none;
   background-color: var(--color-2);
@@ -279,27 +229,12 @@ function banPlayer() {
   vertical-align: middle;
 }
 
-.dropdown {
-  background-color: var(--color-3);
-  padding: 5rem;
-  padding-bottom: 4.7rem;
-  height: 0px;
-  overflow: hidden;
-  transition: height 1.5s ease-in-out;
-}
-
-.dropdown-icon {
-  transition: transform 0.3s ease;
-  margin-left: auto;
-  margin-right: 7px;
-}
-
 .dropdown-buttons-container {
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-start;
-  margin-top: -4rem;
-  margin-left: -4rem
+  flex-direction: column;
+  margin-left: -4rem;
 }
 
 .dropdown-inputs {
@@ -311,7 +246,7 @@ function banPlayer() {
   margin-bottom: 0.5rem;
   border-radius: 0.15rem; 
   border: none;
-  width: 80%;
+  width: 70%;
 }
 
 .dropdown-buttons {
