13e56441e534b84f757a61e0af4885fbddad65b6
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-05-13 13:36:00 +0200

    Update UserReport.svelte

--- ui/src/components/Pages/Report/UserReport.svelte
+++ ui/src/components/Pages/Report/UserReport.svelte
@@ -1,13 +1,15 @@
 <div class="h-screen w-screen flex items-center justify-center">
-    <div class="bg-secondary w-[90rem] h-[70rem]">
-
+    <div class="bg-secondary w-[70rem] h-[70rem]">
+        <!-- Report Header -->
+        <div class="w-full h-[12%] border-b-2 border-tertiary">
+           
+        </div>
         <!-- Chat Container -->
-        <div class="w-full h-[90%]">
+        <div class="w-full h-[78%]">
            
         </div>
         <!-- Input  -->
         <div class="w-full h-[10%] bg-primary flex justify-center items-center">
-            <button class="fas fa-folder-plus fa-lg bg-tertiary w-[6rem] h-full hover:bg-accent"></button>
             <input placeholder="Type your message here..." type="text" class="h-[6.5rem] w-[59rem] ml-6 font-medium bg-transparent">
             <button class="fas fa-paper-plane fa-lg ml-auto bg-tertiary w-[12rem] h-full hover:bg-accent"></button>
         </div>
