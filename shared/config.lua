cc55f5c650be69d9536b760aee32f8c1f4bb46f4
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-05-13 12:47:28 +0200

    Dropdown Data

--- shared/config.lua
+++ shared/config.lua
@@ -12,8 +12,10 @@ Config.Actions = {
         type = "client",
         perms = "mod", 
         dropdown = {
-            { label = "label", type = "input", inputtype = "normal", },
-            { label = "Ban", type = "button", event = "ps-adminmenu:client:banplayer", },
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Reason", type = "input", inputtype = "normal" },
+            { label = "Time", type = "input", inputtype = "normal" },
+            { label = "Ban", type = "button", event = "ps-adminmenu:client:banplayer" },
         },
     },
     { 
@@ -21,28 +23,40 @@ Config.Actions = {
         event = "ps-adminmenu:client:BringPlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Bring", type = "button", event = "" },
+        },
     },
     { 
         label = "Change Weather", 
         event = "ps-adminmenu:client:ChangeWeather", 
         type = "client", 
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Wheather", type = "input", inputtype = "normal" },
+            { label = "Change", type = "button", event = "" },
+        },
     },
     { 
         label = "Clear Inventory", 
         event = "ps-adminmenu:client:ClearInventory", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Clear", type = "button", event = "" },
+        },
     },
     { 
         label = "Clothing Menu", 
         event = "ps-adminmenu:client:ClothingMenu", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Give Menu", type = "button", event = "" },
+        },
     },
     { 
         label = "Customs", 
@@ -54,21 +68,31 @@ Config.Actions = {
         event = "ps-adminmenu:client:FreezePlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Freeze", type = "button", event = "" },
+        },
     },
     { 
         label = "Give Money", 
         event = "ps-adminmenu:client:GiveMoney", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Amount", type = "input", inputtype = "normal" },
+            { label = "Give", type = "button", event = "" },
+        },
     },
     { 
         label = "Give Nui Focus", 
         event = "ps-adminmenu:client:SetNuiFocus", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Submit", type = "button", event = "" },
+        },
     },
     { 
         label = "God", 
@@ -85,21 +109,31 @@ Config.Actions = {
         event = "ps-adminmenu:client:KickPlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Reason", type = "input", inputtype = "normal" },
+            { label = "Kick", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Kill Player", 
         event = "ps-adminmenu:client:KillPlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Kill", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Mute Player", 
         event = "ps-adminmenu:client:MutePlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Mute", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Noclip", 
@@ -111,14 +145,20 @@ Config.Actions = {
         event = "ps-adminmenu:client:OpenInventory", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Submit", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Revive Player", 
         event = "ps-adminmenu:client:Revive Player", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Revive", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Revive All", 
@@ -135,35 +175,52 @@ Config.Actions = {
         event = "ps-adminmenu:client:SetJob", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Job", type = "input", inputtype = "normal" },
+            { label = "Grade", type = "input", inputtype = "normal" },
+            { label = "Sumbit", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Spawn Vehicle", 
         event = "ps-adminmenu:client:SpawnVehicle", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Vehicle", type = "input", inputtype = "normal" },
+            { label = "Sumbit", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Specate Player", 
         event = "ps-adminmenu:client:Spectate Player", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Spectate", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Teleport To Player", 
         event = "ps-adminmenu:client:TeleportToPlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Teleport", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
-        label = "Teleport To Coords", 
+        label = "Teleport To Coordinates", 
         event = "ps-adminmenu:client:TeleportToCoords", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Coordinates", type = "input", inputtype = "normal" },
+            { label = "Teleport", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Teleport To Marker", 
@@ -175,20 +232,30 @@ Config.Actions = {
         event = "ps-adminmenu:client:UnbanPlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Unban", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Uncuff Player", 
         event = "ps-adminmenu:client:UncuffPlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Uncuff", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
     { 
         label = "Warn Player", 
         event = "ps-adminmenu:client:WarnPlayer", 
         type = "client",
         perms = "mod", 
-        dropdown = {},
+        dropdown = {
+            { label = "Player ID", type = "input", inputtype = "normal" },
+            { label = "Reason", type = "input", inputtype = "normal" },
+            { label = "Warn", type = "button", event = "ps-adminmenu:client:banplayer" },
+        },
     },
 }
