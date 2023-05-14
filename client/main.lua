dcc82bd4511e50e14b18fc8e5f6f36667741046e
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-05-14 11:32:07 +0200

    Create main.lua

---
+++ client/main.lua
@@ -0,0 +1,36 @@
+QBCore = exports['qb-core']:GetCoreObject()
+
+local function toggleUI(bool)
+	SetNuiFocus(bool, bool)
+	SendNUIMessage({
+		action = "setVisible",
+		data = bool
+	})
+end
+
+RegisterCommand("admin", function()
+	toggleUI(true)
+	SendNUIMessage({
+		action = "setActionData",
+		data = Config.Actions
+	})
+end, false)
+
+RegisterNUICallback("hideUI", function()
+    toggleUI(false)
+end)
+
+RegisterNUICallback("normalButton", function(data, cb) 
+    local event = data.event 
+    local type = data.type
+	if event and type then 
+		if type == "client" then 
+			TriggerEvent(event)
+		elseif type == "server" then 
+			TriggerServerEvent(event)
+		elseif type == "command" then 
+			ExecuteCommand(event)
+		end
+	end
+    cb("ok")
+end)
