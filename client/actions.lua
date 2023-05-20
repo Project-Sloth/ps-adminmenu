c6936df2a48872d5d68ffa3ffc4e17661b69db2e
Lenzh <lorenzo0109@hotmail.nl>
2023-05-21 00:34:27 +0200

    Admincar

--- client/actions.lua
+++ client/actions.lua
@@ -1,8 +1,32 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
 -- Admin Car
+local function getVehicleFromVehList(hash)
+	for _, v in pairs(QBCore.Shared.Vehicles) do
+		if hash == v.hash then
+			return v.model
+		end
+	end
+end
 RegisterNetEvent('ps-adminmenu:client:Admincar', function(data)
 	print("AdminCarTriggerd")
+    local ped = PlayerPedId()
+    local veh = GetVehiclePedIsIn(ped)
+
+    if veh ~= nil and veh ~= 0 then
+        local plate = QBCore.Functions.GetPlate(veh)
+        local props = QBCore.Functions.GetVehicleProperties(veh)
+        local hash = props.model
+        local vehname = getVehicleFromVehList(hash)
+        if QBCore.Shared.Vehicles[vehname] ~= nil and next(QBCore.Shared.Vehicles[vehname]) ~= nil then
+            TriggerServerEvent('ps-adminmenu:server:SaveCar', props, QBCore.Shared.Vehicles[vehname], GetHashKey(veh), plate)
+        else
+            QBCore.Functions.Notify(Lang:t("error.cannot_store_veh"), 'error')
+           
+        end
+    else
+        QBCore.Functions.Notify(Lang:t("error.not_in_veh"), 'error')
+    end
 end)
 
 -- Invisible
