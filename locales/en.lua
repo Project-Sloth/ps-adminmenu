700bd1c4c3c4aaaa202933eb4d4602ad524c7856
Lenzh <lorenzo0109@hotmail.nl>
2023-05-21 00:35:36 +0200

    admincar locales

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,8 @@
 local Translations = {
     error = {
         ["u_veh_owner"] = "This vehicle is already yours..",
+        ['cannot_store_veh'] = "Cannot store this car in your garage",
+        ["not_in_veh"] = "You are not in a vehicle..",
     },
     success = {
         ["veh_owner"] = "The vehicle is now yours!",
