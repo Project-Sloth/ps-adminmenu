b309a9d5fdf43188d0fd76799a8a6dd7b4f63b82
Lenzh <lorenzo0109@hotmail.nl>
2023-05-21 05:09:24 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -4,9 +4,14 @@ local Translations = {
         ['cannot_store_veh'] = "Cannot store this car in your garage",
         ["not_in_veh"] = "You are not in a vehicle..",
         ["no_perms"] = "You do not have permission to do this",
+        ["missing_args"] = 'Not every argument has been entered (x, y, z)',
+        ["not_online"] = 'Player not online',
+        ['no_waypoint'] = 'No Waypoint Set.',
+        ["tp_error"] = 'Error While Teleporting.',
     },
     success = {
         ["veh_owner"] = "The vehicle is now yours!",
+        ["teleported_waypoint"] = 'Teleported To Waypoint.',
     },
     info = {
         ["godmode"] = "Godmode is %{value}",
