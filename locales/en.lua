3d1b57242b8bbdedf75d4843991989c701c52b59
Lenzh <lorenzo0109@hotmail.nl>
2023-05-21 02:19:20 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -3,12 +3,16 @@ local Translations = {
         ["u_veh_owner"] = "This vehicle is already yours..",
         ['cannot_store_veh'] = "Cannot store this car in your garage",
         ["not_in_veh"] = "You are not in a vehicle..",
+        ["no_perms"] = "You do not have permission to do this",
     },
     success = {
         ["veh_owner"] = "The vehicle is now yours!",
     },
     info = {
-
+        ["godmode"] = "Godmode is %{value}",
+        ["invisible"] = "Invisible: %{value}",
+        ["banreason"] = 'Reason: %{reason}, until %{lenght}',
+        ["weatherType"] = "Weather is changed to: %{value}",
     },
 
 }
