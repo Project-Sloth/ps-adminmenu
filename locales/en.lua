d1569d2486721314eb798bf290d013489dfa4c2b
Lenzh <lorenzo0109@hotmail.nl>
2023-05-22 03:19:41 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -12,6 +12,7 @@ local Translations = {
     success = {
         ["veh_owner"] = "The vehicle is now yours!",
         ["teleported_waypoint"] = 'Teleported To Waypoint.',
+        ["invcleared"] = "%{player}'s inventory is cleared"
     },
     info = {
         ["godmode"] = "Godmode is %{value}",
