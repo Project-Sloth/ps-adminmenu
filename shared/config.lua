01cc9775bd3b0d0616c225955faf339dfa6cf02d
Lenzh <lorenzo0109@hotmail.nl>
2023-07-31 12:15:29 +0200

    Perms listed

--- shared/config.lua
+++ shared/config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.InventoryUsage =  "qb" -- qb(for qb-inventory) lj(for lj-inventory) or ox(for ox_inventory) [Inventory system]
 
-Config.FuelScript =  "LegacyFuel" -- LegacyFuel / qb-fuel / ps-fuel / lj-fuel [Car spawning/fixing]
+Config.FuelScript =  "ps-fuel" -- LegacyFuel / qb-fuel / ps-fuel / lj-fuel [Car spawning/fixing]
 
 Config.Actions = {
     {
@@ -18,7 +18,7 @@ Config.Actions = {
             { label = "Player ID", InputType = "players" },
             { label = "Reason", InputType = "input" },
             { label = "Time", InputType = "options",
-               options = {
+            options = {
                     { label = "10 Minutes", value = "600"},
                     { label = "30 Minutes", value = "1800"},
                     { label = "1 Hour", value = "3600"},
@@ -37,7 +37,7 @@ Config.Actions = {
         label = "Bring Player",
         perms = "mod",
         dropdown = {
-            { label = "Player ID", type = "input", InputType = "input" },
+            { label = "Player ID", type = "input", InputType = "players" },
             { label = "Bring", InputType = "button", type = "server", event = "ps-adminmenu:server:BringPlayer" },
         },
     },
@@ -45,7 +45,7 @@ Config.Actions = {
         label = "Kick Player",
         perms = "admin",
         dropdown = {
-            { label = "Player ID", type = "input", InputType = "input" },
+            { label = "Player ID", type = "input", InputType = "players" },
             { label = "Reason", type = "input", InputType = "input" },
             { label = "Kick", InputType = "button", type = "server", event = "ps-adminmenu:server:KickPlayer" },
         },
@@ -189,6 +189,7 @@ Config.Actions = {
     },
     {
         label = "Delete Vehicle",
+        perms = "mod",
         event = "dv",
         type = "command"
     },
@@ -211,21 +212,25 @@ Config.Actions = {
     {
         label = "God",
         event = "ps-adminmenu:client:ToggleGodmode",
+        perms = "mod",
         type = "client"
     },
     {
         label = "Invisible",
         event = "ps-adminmenu:client:ToggleInvisible",
+        perms = "mod",
         type = "client"
     },
     {
         label = "Toggle Blackout",
         event = "ps-adminmenu:server:ToggleBlackout",
+        perms = "mod",
         type = "server"
     },
     {
         label = "Toggle Duty",
         event = "ps-adminmenu:client:ToggleDuty",
+        perms = "mod",
         type = "client"
     },
     {
@@ -270,7 +275,8 @@ Config.Actions = {
     {
         label = "Noclip",
         event = "ps-adminmenu:client:ToggleNoClip",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
     {
         label = "Open Inventory",
@@ -345,14 +351,15 @@ Config.Actions = {
         label = "Revive Player",
         perms = "mod",
         dropdown = {
-            { label = "Player ID", type = "input", InputType = "input" },
+            { label = "Player ID", type = "input", InputType = "players" },
             { label = "Revive", InputType = "button", type = "server", event = "ps-adminmenu:server:Revive" },
         },
     },
     {
         label = "Revive All",
         event = "ps-adminmenu:server:ReviveAll",
-        type = "server"
+        type = "server",
+        perms = "admin",
     },
     {
         label = "Set Job",
@@ -466,7 +473,7 @@ Config.Actions = {
         label = "Teleport To Player",
         perms = "mod",
         dropdown = {
-            { label = "Player ID", type = "input", InputType = "input" },
+            { label = "Player ID", type = "input", InputType = "players" },
             { label = "Teleport", InputType = "button", type = "server", event = "ps-adminmenu:server:TeleportToPlayer" },
         },
     },
@@ -483,37 +490,44 @@ Config.Actions = {
     {
         label = "Teleport To Marker",
         event = "ps-adminmenu:client:TeleportToMarker",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
     {
         label = "Teleport Back",
         event = "ps-adminmenu:client:TeleportBack",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
     {
         label = "Vehicle Dev Menu",
         event = "ps-adminmenu:client:ToggleVehDevMenu",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
     {
         label = "Toggle Coords",
         event = "ps-adminmenu:client:ToggleCoords",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
     {
         label = "Toggle Blips",
         event = "ps-adminmenu:client:toggleBlips",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
     {
         label = "Toggle Names",
         event = "ps-adminmenu:client:toggleNames",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
     {
         label = "Max Mod Vehicle",
         event = "ps-adminmenu:client:maxmodVehicle",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
     {
         label = "Toggle Cuffs",
@@ -535,7 +549,8 @@ Config.Actions = {
     {
         label = "Infinite Ammo",
         event = "ps-adminmenu:client:setInfiniteAmmo",
-        type = "client"
+        type = "client",
+        perms = "mod",
     },
 }
 
