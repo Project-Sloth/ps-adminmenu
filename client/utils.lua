1d796e72fac4a8882cde103feebbc75627598b4e
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-08-26 21:36:18 +0200

    Update utils.lua

--- client/utils.lua
+++ client/utils.lua
@@ -11,3 +11,16 @@ function CheckPerms(perms)
 
     return hasPerms
 end
+
+function Draw2DText(content, font, colour, scale, x, y)
+    SetTextFont(font)
+    SetTextScale(scale, scale)
+    SetTextColour(colour[1],colour[2],colour[3], 255)
+    SetTextEntry("STRING")
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextDropShadow()
+    SetTextEdge(4, 0, 0, 0, 255)
+    SetTextOutline()
+    AddTextComponentString(content)
+    DrawText(x, y)
+end
