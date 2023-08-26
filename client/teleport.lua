d9d47c8bde67a57b32620c85f18b548289033fbd
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-08-26 22:56:43 +0200

    Update teleport.lua

--- client/teleport.lua
+++ client/teleport.lua
@@ -1,37 +1,32 @@
 local lastCoords
 
-local function teleport(vehicle, x, y, z)
-    local ped = PlayerPedId()
-    if vehicle then
-        return SetPedCoordsKeepVehicle(ped, x, y, z)
+local function teleport(x, y, z)
+    if cache.vehicle then
+        return SetPedCoordsKeepVehicle(cache.ped, x, y, z)
     end
-    SetEntityCoords(ped, x, y, z, false, false, false, false)
+    SetEntityCoords(cache.ped, x, y, z, false, false, false, false)
 end
 
 -- Teleport to player
 RegisterNetEvent('ps-adminmenu:client:TeleportToPlayer', function(coords)
-    local ped = PlayerPedId()
-    lastCoords = GetEntityCoords(ped)
-    SetPedCoordsKeepVehicle(ped, coords.x, coords.y, coords.z)
+    lastCoords = GetEntityCoords(cache.ped)
+    SetPedCoordsKeepVehicle(cache.ped, coords.x, coords.y, coords.z)
 end)
 
 -- Teleport to coords
 RegisterNetEvent('ps-adminmenu:client:TeleportToCoords', function(x, y, z, h)
-    local ped = PlayerPedId()
-    lastCoords = GetEntityCoords(ped)
-    SetPedCoordsKeepVehicle(ped, x, y, z)
-    SetEntityHeading(ped, h or GetEntityHeading(ped))
+    lastCoords = GetEntityCoords(cache.ped)
+    SetPedCoordsKeepVehicle(cache.ped, x, y, z)
+    SetEntityHeading(cache.ped, h or GetEntityHeading(cache.ped))
 end)
 
 -- Teleport back
-RegisterNetEvent('ps-adminmenu:client:TeleportBack', function(coords, _,perms)
+RegisterNetEvent('ps-adminmenu:client:TeleportBack', function(_, _, perms)
     if not PermsCheck(perms) then return end
 
-    local ped = PlayerPedId()
-    local vehicle = GetVehiclePedIsIn(ped, false)
     if lastCoords then
-        local currentCoords = GetEntityCoords(ped)
-        teleport(vehicle, lastCoords.x, lastCoords.y, lastCoords.z)
-        lastCoords = currentCoords
+        local coords = GetEntityCoords(cache.ped)
+        teleport(lastCoords.x, lastCoords.y, lastCoords.z)
+        lastCoords = coords
     end
 end)
