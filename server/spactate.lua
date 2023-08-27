743855e81383b53ab391b1c2c858b14bfd9785a4
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-08-27 23:02:09 +0200

    Create spactate.lua

---
+++ server/spactate.lua
@@ -0,0 +1,38 @@
+local spectating = {}
+
+RegisterNetEvent('ps-adminmenu:server:SpectateTarget', function(data, selectedData)
+    if not CheckPerms(data.perms) then return end
+    local player = selectedData["Player"].value
+
+    local type = "1"
+    if player == source then return QBCore.Functions.Notify(source, locale("cant_spectate_yourself"), 'error', 7500) end
+    if spectating[source] then type = "0" end
+    TriggerEvent('ps-adminmenu:spectate', player, type == "1", source)
+    CheckRoutingbucket(source, player)
+end)
+
+AddEventHandler('ps-adminmenu:spectate', function(target, on, source)
+    local tPed = GetPlayerPed(target)
+    if DoesEntityExist(tPed) then
+        if not on then
+            TriggerClientEvent('ps-adminmenu:cancelSpectate', source)
+            spectating[source] = false
+            FreezeEntityPosition(GetPlayerPed(source), false)
+            TriggerClientEvent('ps-adminmenu:client:toggleNames', source)
+        elseif on then
+            TriggerClientEvent('ps-adminmenu:requestSpectate', source, NetworkGetNetworkIdFromEntity(tPed), target, GetPlayerName(target))
+            spectating[source] = true
+            TriggerClientEvent('ps-adminmenu:client:toggleNames', source)
+        end
+    end
+end)
+
+RegisterNetEvent('ps-adminmenu:spectate:teleport', function(target)
+    local source = source
+    local ped = GetPlayerPed(target)
+    if DoesEntityExist(ped) then
+        local targetCoords = GetEntityCoords(ped)
+        SetEntityCoords(GetPlayerPed(source), targetCoords.x, targetCoords.y, targetCoords.z - 10)
+        FreezeEntityPosition(GetPlayerPed(source), true)
+    end
+end)
