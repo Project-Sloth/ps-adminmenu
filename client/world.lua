8e6be271d761b0249cfe023399a6245aeb9ea2b3
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-08-31 22:37:02 +0200

    Change Time, Change weather

---
+++ client/world.lua
@@ -0,0 +1,18 @@
+-- Changes the time
+RegisterNetEvent('ps-adminmenu:client:ChangeTime', function(data, selectedData)
+    if not CheckPerms(data.perms) then return end
+    local time = selectedData["Time Events"].value
+
+    if not time then return end
+
+    TriggerServerEvent('qb-weathersync:server:setTime', time, 00)
+end)
+
+-- Changes the weather
+RegisterNetEvent('ps-adminmenu:client:ChangeWeather', function(data, selectedData)
+    if not CheckPerms(data.perms) then return end
+    local weather = selectedData["Weather"].value
+
+    TriggerServerEvent('qb-weathersync:server:setWeather', weather)
+end)
+
