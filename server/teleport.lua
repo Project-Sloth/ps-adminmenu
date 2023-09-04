b68e286116d13ce833cac150741a77aefb3fa1d8
Lenzh <lorenzo0109@hotmail.nl>
2023-09-04 22:57:55 +0100

    added bring player function

--- server/teleport.lua
+++ server/teleport.lua
@@ -11,3 +11,16 @@ RegisterNetEvent('ps-adminmenu:server:TeleportToPlayer', function(data, selected
         TriggerClientEvent('QBCore:Notify', source, locale('not_online'), 'error')
     end
 end)
+
+-- Bring Player
+RegisterNetEvent('ps-adminmenu:server:BringPlayer', function(data, selectedData)
+    if not CheckPerms(data.perms) then return end
+    local src = source
+    local targetPed = selectedData["Player"].value
+
+    local admin = GetPlayerPed(src)
+    local coords = GetEntityCoords(admin)
+    local target = GetPlayerPed(targetPed)
+    CheckRoutingbucket(targetPed, src)
+    SetEntityCoords(target, coords)
+end)
