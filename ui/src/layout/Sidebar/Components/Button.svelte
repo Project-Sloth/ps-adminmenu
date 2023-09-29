db97007130e284c3d44f7deee15aa4692e6f6436
Lenzh <lorenzo0109@hotmail.nl>
2023-09-29 22:37:37 +0100

    sidebar tooltips

--- ui/src/layout/Sidebar/Components/Button.svelte
+++ ui/src/layout/Sidebar/Components/Button.svelte
@@ -8,7 +8,31 @@
 
 <button
 	title={tooltiptext}
-	class="w-[4vh] h-[4vh] rounded-[0.5vh] hover:bg-tertiary {$ACTIVE_PAGE == value ? 'bg-tertiary' : ''}"
+	class="w-[4vh] h-[4vh] rounded-[0.5vh] hover:bg-tertiary {$ACTIVE_PAGE == value ? 'bg-tertiary' : ''}
+		relative
+		before:content-[attr(data-tip)]
+		before:absolute
+		before:-right-3 before:top-1/2
+		before:w-max before:max-w-xs
+		before:-translate-x-full before:-translate-y-1/2
+		before:bg-tertiary before:text-white
+		before:rounded-md before:opacity-0
+		before:translate-all
+
+		after:absolute
+		after:-right-3 after:top-1/2
+		after:-translate-x-0 after:-translate-y-1/2
+		after:h-0 after:w-0
+		after:border-t-transparent
+		after:border-l-transparent
+		after:border-b-transparent
+		after:border-r-tertiary
+		after:opacity-0
+		after:transition-all
+
+		hover:before:opacity-100 hover:after:opacity-100
+		"
+		data-tip={tooltiptext}
 	on:click={() => {
 		ACTIVE_PAGE.set(value)
 	}}
--- ui/src/layout/Sidebar/Sidebar.svelte
+++ ui/src/layout/Sidebar/Sidebar.svelte
@@ -30,8 +30,32 @@
 		<Button tooltiptext={nav.value} icon={nav.icon} value={nav.value} />
 	{/each}
 
-	<button title='Dev Mode'
-		class="w-[4vh] h-[4vh] rounded-[0.5vh] hover:bg-tertiary {$DEV_MODE ? "text-accent" : ""}"
+	<button
+		class="w-[4vh] h-[4vh] rounded-[0.5vh] hover:bg-tertiary {$DEV_MODE ? "text-accent" : ""}
+		relative
+		before:content-[attr(data-tip)]
+		before:absolute
+		before:-right-3 before:top-1/2
+		before:w-max before:max-w-xs
+		before:-translate-x-full before:-translate-y-1/2
+		before:bg-tertiary before:text-white
+		before:rounded-md before:opacity-0
+		before:translate-all
+
+		after:absolute
+		after:-right-3 after:top-1/2
+		after:-translate-x-0 after:-translate-y-1/2
+		after:h-0 after:w-0
+		after:border-t-transparent
+		after:border-l-transparent
+		after:border-b-transparent
+		after:border-r-tertiary
+		after:opacity-0
+		after:transition-all
+
+		hover:before:opacity-100 hover:after:opacity-100
+		"
+		data-tip='Dev Mode'
 		on:click={() => DEV_MODE.update((wide) => !wide)}
 	>
 		<i class="fas fa-code"></i>
