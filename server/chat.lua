09d28a224075376ac6a854cd48ed84bf1d62ac87
uyuyorum {um} <56808589+alp1x@users.noreply.github.com>
2023-11-25 12:49:32 +0300

    GetAllCommands | Chat in promise failed fix cb | button hover padding

--- server/chat.lua
+++ server/chat.lua
@@ -2,25 +2,23 @@ local messages = {}
 
 -- Staff Chat
 RegisterNetEvent('ps-adminmenu:server:sendMessageServer', function(message, citizenid, fullname)
+    if not CheckPerms('mod') then return end
+
     local time = os.time() * 1000
     local players = QBCore.Functions.GetPlayers()
 
     for i = 1, #players, 1 do
         local player = players[i]
-        if QBCore.Functions.HasPermission(player, 'mod') or IsPlayerAceAllowed(player, 'command') then
             if QBCore.Functions.IsOptin(player) then
                 QBCore.Functions.Notify(player, locale("new_staffchat", 'inform', 7500))
             end
         end
-    end
 
     messages[#messages + 1] = { message = message, citizenid = citizenid, fullname = fullname, time = time }
 end)
 
-lib.callback.register('ps-adminmenu:callback:GetMessages', function(source)
-    if QBCore.Functions.HasPermission(player, 'mod') or IsPlayerAceAllowed(player, 'command') then
-        return messages
-    end
 
-    return {}
+lib.callback.register('ps-adminmenu:callback:GetMessages', function()
+    if not CheckPerms('mod') then return {} end
+    return messages
 end)
--- ui/src/pages/Commands/Commands.svelte
+++ ui/src/pages/Commands/Commands.svelte
@@ -18,10 +18,10 @@
 	/>
 	<div class="w-full h-[84%] flex flex-col gap-[1vh] mt-[1vh] overflow-auto">
 		{#if $COMMANDS}
-		<small class="font-medium">Total Commands: {SortedCommands.length}</small>
 			{#if $COMMANDS && $COMMANDS.filter((commands) => commands.name.toLowerCase().includes(search.toLowerCase())).length === 0}
 				<div class="text-tertiary text-center text-[1.7vh] font-medium mt-[1vh]">No Commands Found.</div>
 			{:else}
+				<small class="font-medium">Total Commands: {SortedCommands.length}</small>
 				{#each SortedCommands.filter((commands) => commands.name.toLowerCase().includes(search.toLowerCase())) as commands}
 					<CommandsCard label={commands.name} />
 				{/each}
