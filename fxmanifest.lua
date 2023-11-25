75935b7b17158cd999fea694a4d518dbfeb4cbfc
Walter <64671092+Walter-00@users.noreply.github.com>
2023-11-25 16:56:03 +0100

    1.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'cerulean'
 game "gta5"
 
 author "Project Sloth & OK1ez"
-version '1.0.9'
+version '1.1.0'
 description 'Admin Menu'
 repository 'https://github.com/Project-Sloth/ps-adminmenu'
 
