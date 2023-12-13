201704b5f32b67e4f8fc9c8f1c490e548a12047f
LeSiiN <103898231+LeSiiN@users.noreply.github.com>
2023-12-13 14:43:16 +0100

    fix for copy coords
    fix for copy coords

--- client/world.lua
+++ client/world.lua
@@ -17,3 +17,45 @@ RegisterNetEvent('ps-adminmenu:client:ChangeWeather', function(data, selectedDat
 
     TriggerServerEvent('qb-weathersync:server:setWeather', weather)
 end)
+
+RegisterNetEvent('ps-adminmenu:client:copyToClipboard', function(data, selectedData)
+    local data = CheckDataFromKey(data)
+    if not data or not CheckPerms(data.perms) then return end
+
+    local dropdown = selectedData["Copy Coords"].value
+    local ped = PlayerPedId()
+    local string = nil
+    if dropdown == 'vector2' then
+        local coords = GetEntityCoords(ped)
+        local x = QBCore.Shared.Round(coords.x, 2)
+        local y = QBCore.Shared.Round(coords.y, 2)
+        string = "vector2(".. x ..", ".. y ..")"
+        QBCore.Functions.Notify(locale("copy_vector2"), 'success')
+    elseif dropdown == 'vector3' then
+        local coords = GetEntityCoords(ped)
+        local x = QBCore.Shared.Round(coords.x, 2)
+        local y = QBCore.Shared.Round(coords.y, 2)
+        local z = QBCore.Shared.Round(coords.z, 2)
+        string = "vector3(".. x ..", ".. y ..", ".. z ..")"
+        QBCore.Functions.Notify(locale("copy_vector3"), 'success')
+    elseif dropdown == 'vector4' then
+        local coords = GetEntityCoords(ped)
+        local x = QBCore.Shared.Round(coords.x, 2)
+        local y = QBCore.Shared.Round(coords.y, 2)
+        local z = QBCore.Shared.Round(coords.z, 2)
+        local heading = GetEntityHeading(ped)
+        local h = QBCore.Shared.Round(heading, 2)
+        string = "vector4(".. x ..", ".. y ..", ".. z ..", ".. h ..")"
+        QBCore.Functions.Notify(locale("copy_vector4"), 'success')
+    elseif dropdown == 'heading' then
+        local heading = GetEntityHeading(ped)
+        local h = QBCore.Shared.Round(heading, 2)
+        string = h
+        QBCore.Functions.Notify(locale("copy_heading"), 'success')
+    elseif string == nil then 
+        QBCore.Functions.Notify(locale("empty_input"), 'error')
+    end
+
+    lib.setClipboard(string)
+
+end)
