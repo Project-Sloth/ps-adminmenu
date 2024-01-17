8eff52b5c6a42422fa769059000c92d10799ed9d
Kenta <102387086+KentaKK@users.noreply.github.com>
2024-01-17 21:14:56 +0100

    Update chat.lua

--- server/chat.lua
+++ server/chat.lua
@@ -2,23 +2,15 @@ local messages = {}
 
 -- Staff Chat
 RegisterNetEvent('ps-adminmenu:server:sendMessageServer', function(message, citizenid, fullname)
-    if not CheckPerms('mod') then return end
+    if not CheckPerms('admin') then return end
 
     local time = os.time() * 1000
-    local players = QBCore.Functions.GetPlayers()
-
-    for i = 1, #players, 1 do
-        local player = players[i]
-            if QBCore.Functions.IsOptin(player) then
-                QBCore.Functions.Notify(player, locale("new_staffchat", 'inform', 7500))
-            end
-        end
-
+    TriggerClientEvent('chat:addMessage', -1, _U("new_staffchat", 'inform', 7500))
     messages[#messages + 1] = { message = message, citizenid = citizenid, fullname = fullname, time = time }
 end)
 
 
 lib.callback.register('ps-adminmenu:callback:GetMessages', function()
-    if not CheckPerms('mod') then return {} end
+    if not CheckPerms('admin') then return {} end
     return messages
 end)
