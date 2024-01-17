09113e9bbd6bbfaaebdbe2e1c4598ecbf57c2f67
Kenta <102387086+KentaKK@users.noreply.github.com>
2024-01-17 21:18:20 +0100

    Update vehicle.lua

--- server/vehicle.lua
+++ server/vehicle.lua
@@ -1,24 +1,22 @@
 -- Admin Car
-RegisterNetEvent('ps-adminmenu:server:SaveCar', function(mods, vehicle, _, plate)
+RegisterNetEvent('ps-adminmenu:server:SaveCar', function(vehicle)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local result = MySQL.query.await('SELECT plate FROM player_vehicles WHERE plate = ?', { plate })
+    local Player = ESX.GetPlayerFromId(src)
+    local plate = vehicle.plate
+    local result = MySQL.query.await('SELECT plate FROM owned_vehicles WHERE plate = ?', { plate })
 
     if result[1] == nil then
-        MySQL.insert(
-        'INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        MySQL.prepare(
+        'INSERT INTO owned_vehicles (`owner`, `vehicle`, `plate`, `stored`) VALUES (?, ?, ?, ?)',
             {
-                Player.PlayerData.license,
-                Player.PlayerData.citizenid,
-                vehicle.model,
-                vehicle.hash,
-                json.encode(mods),
+                Player.identifier,
+                json.encode(vehicle),
                 plate,
                 0
             })
-        TriggerClientEvent('QBCore:Notify', src, locale("veh_owner"), 'success', 5000)
+        TriggerClientEvent('esx:showNotification', src, _U("veh_owner"), 'success', 5000)
     else
-        TriggerClientEvent('QBCore:Notify', src, locale("u_veh_owner"), 'error', 3000)
+        TriggerClientEvent('esx:showNotification', src, _U("u_veh_owner"), 'error', 3000)
     end
 end)
 
@@ -28,72 +26,63 @@ RegisterNetEvent("ps-adminmenu:server:givecar", function(data, selectedData)
 
     local data = CheckDataFromKey(data)
     if not data or not CheckPerms(data.perms) then
-        QBCore.Functions.Notify(src, locale("no_perms"), "error", 5000)
+        TriggerClientEvent('esx:showNotification', src, _U("no_perms"), "error", 5000)
         return
     end
 
+    if not selectedData['Plate (Optional)'].value then return TriggerClientEvent('esx:showNotification', src, "No plate", "error", 5000) end
+
+    local plate = selectedData['Plate (Optional)'] and selectedData['Plate (Optional)'].value
     local vehmodel = selectedData['Vehicle'].value
-    local vehicleData = lib.callback.await("ps-adminmenu:client:getvehData", src, vehmodel)
+    local vehicleData = lib.callback.await("ps-adminmenu:client:getvehData", src, vehmodel, plate)
 
     if not next(vehicleData) then
         return
     end
 
-    local tsrc = selectedData['Player'].value
-    local plate = selectedData['Plate (Optional)'] and selectedData['Plate (Optional)'].value or vehicleData.plate
-    local garage = selectedData['Garage (Optional)'] and selectedData['Garage (Optional)'].value or Config.DefaultGarage
-    local Player = QBCore.Functions.GetPlayer(tsrc)
 
-    if plate and #plate < 1 then
-        plate = vehicleData.plate
-    end
+    local tsrc = selectedData['Player'].value
+    local garage = selectedData['Garage (Optional)'] and selectedData['Garage (Optional)'].value or Config.DefaultGarage
+    local Player = ESX.GetPlayerFromId(tsrc)
 
     if garage and #garage < 1 then
         garage = Config.DefaultGarage
     end
 
     if plate:len() > 8 then
-        QBCore.Functions.Notify(src, locale("plate_max"), "error", 5000)
+        TriggerClientEvent('esx:showNotification', src, _U("plate_max"), "error", 5000)
         return
     end
 
     if not Player then
-        QBCore.Functions.Notify(src, locale("not_online"), "error", 5000)
+        TriggerClientEvent('esx:showNotification', src, _U("not_online"), "error", 5000)
         return
     end
 
     if CheckAlreadyPlate(plate) then
-        QBCore.Functions.Notify(src, locale("givecar.error.plates_alreadyused", plate:upper()), "error", 5000)
+        TriggerClientEvent('esx:showNotification', src, _U("already_plate", plate:upper()), "error", 5000)
         return
     end
 
-    MySQL.insert(
-    'INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, garage, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+    MySQL.prepare('INSERT INTO `owned_vehicles` (`owner`, `vehicle`, `garage`, `plate`, `stored`) VALUES (?, ?, ?, ?, ?)',
         {
-            Player.PlayerData.license,
-            Player.PlayerData.citizenid,
-            vehmodel,
-            joaat(vehmodel),
+            Player.identifier,
             json.encode(vehicleData),
-            plate,
             garage,
+            plate,
             1
         })
 
-    QBCore.Functions.Notify(src,
-        locale("givecar.success.source", QBCore.Shared.Vehicles[vehmodel].name,
-            ("%s %s"):format(Player.PlayerData.charinfo.firstname, Player.PlayerData.charinfo.lastname)), "success", 5000)
-    QBCore.Functions.Notify(Player.PlayerData.source, locale("givecar.success.target", plate:upper(), garage), "success",
-        5000)
+        TriggerClientEvent('esx:showNotification', src, _U("givecar.success.source", vehmodel, Player.getName()), "success", 5000) --("%s %s"):format(Player.getName())), "success", 5000)
+        TriggerClientEvent('esx:showNotification', Player.source, _U("givecar.success.target", plate:upper(), garage), "success", 5000)
 end)
 
 -- Give Car
 RegisterNetEvent("ps-adminmenu:server:SetVehicleState", function(data, selectedData)
     local src = source
-
     local data = CheckDataFromKey(data)
     if not data or not CheckPerms(data.perms) then
-        QBCore.Functions.Notify(src, locale("no_perms"), "error", 5000)
+        TriggerClientEvent('esx:showNotification', src, _U("no_perms"), "error", 5000)
         return
     end
 
@@ -101,18 +90,23 @@ RegisterNetEvent("ps-adminmenu:server:SetVehicleState", function(data, selectedD
     local state = tonumber(selectedData['State'].value)
 
     if plate:len() > 8 then
-        QBCore.Functions.Notify(src, locale("plate_max"), "error", 5000)
+        TriggerClientEvent('esx:showNotification', src, _U("plate_max"), "error", 5000)
         return
     end
 
     if not CheckAlreadyPlate(plate) then
-        QBCore.Functions.Notify(src, locale("plate_doesnt_exist"), "error", 5000)
+        TriggerClientEvent('esx:showNotification', src, _U("plate_doesnt_exist"), "error", 5000)
         return
     end
 
-    MySQL.update('UPDATE player_vehicles SET state = ?, depotprice = ? WHERE plate = ?', { state, 0, plate })
+    MySQL.update('UPDATE `owned_vehicles` SET `stored` = ? WHERE `plate` = ?', {state, plate})
 
-    QBCore.Functions.Notify(src, locale("state_changed"), "success", 5000)
+    TriggerClientEvent('esx:showNotification', src, _U("state_changed"), "success", 5000)
+end)
+
+RegisterNetEvent("ps-adminmenu:server:DeleteObj", function(netId)
+    local obj = NetworkGetEntityFromNetworkId(netId)
+    DeleteEntity(obj)
 end)
 
 -- Change Plate
@@ -123,15 +117,20 @@ RegisterNetEvent('ps-adminmenu:server:ChangePlate', function(newPlate, currentPl
         exports.ox_inventory:UpdateVehicle(currentPlate, newPlate)
     end
 
-    MySQL.Sync.execute('UPDATE player_vehicles SET plate = ? WHERE plate = ?', { newPlate, currentPlate })
-    MySQL.Sync.execute('UPDATE trunkitems SET plate = ? WHERE plate = ?', { newPlate, currentPlate })
-    MySQL.Sync.execute('UPDATE gloveboxitems SET plate = ? WHERE plate = ?', { newPlate, currentPlate })
+    MySQL.update('UPDATE `owned_vehicles` SET `plate` = ? WHERE `plate` = ?', { newPlate, currentPlate })
+    --MySQL.update('UPDATE trunkitems SET plate = ? WHERE plate = ?', { newPlate, currentPlate })
+    --MySQL.update('UPDATE gloveboxitems SET plate = ? WHERE plate = ?', { newPlate, currentPlate })
 end)
 
-lib.callback.register('ps-adminmenu:server:GetVehicleByPlate', function(source, plate)
-    local result = MySQL.query.await('SELECT vehicle FROM player_vehicles WHERE plate = ?', { plate })
-    local veh = result[1] and result[1].vehicle or {}
+lib.callback.register('ps-adminmenu:server:GetVehicleByPlate', function(_, plate)
+    local result = MySQL.query.await('SELECT `vehicle` FROM `owned_vehicles` WHERE `plate` = ?', { plate })
+    local veh = result[1] and json.decode(result[1].vehicle) or {}
     return veh
+end)
+
+lib.callback.register('ps-adminmenu:callback:GetVehicles', function()
+    local result = MySQL.query.await('SELECT * FROM `vehicles`')
+    return result
 end)
 
 -- Fix Vehicle for player
@@ -140,13 +139,12 @@ RegisterNetEvent('ps-adminmenu:server:FixVehFor', function(data, selectedData)
     if not data or not CheckPerms(data.perms) then return end
     local src = source
     local playerId = selectedData['Player'].value
-    local Player = QBCore.Functions.GetPlayer(tonumber(playerId))
+    local Player = ESX.GetPlayerFromId(tonumber(playerId))
+    local name = Player.getName()
     if Player then
-        local name = Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname
-        TriggerClientEvent('iens:repaira', Player.PlayerData.source)
-        TriggerClientEvent('vehiclemod:client:fixEverything', Player.PlayerData.source)
-        QBCore.Functions.Notify(src, locale("veh_fixed", name), 'success', 7500)
+        TriggerClientEvent('ps-adminmenu:client:fixEverything', Player.source)
+        TriggerClientEvent('esx:showNotification', src, _U("veh_fixed", name), 'success', 7500)
     else
-        TriggerClientEvent('QBCore:Notify', src, locale("not_online"), "error")
+        TriggerClientEvent('esx:showNotification', src, _U("not_online"), "error")
     end
 end)
