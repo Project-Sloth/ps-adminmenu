99e3d1762f8a00af86e767b06fbad5f4641ea9c4
Kenta <102387086+KentaKK@users.noreply.github.com>
2024-01-17 21:17:55 +0100

    Update trolls.lua

--- server/trolls.lua
+++ server/trolls.lua
@@ -1,5 +1,6 @@
 -- Freeze Player
 local frozen = false
+
 RegisterNetEvent('ps-adminmenu:server:FreezePlayer', function(data, selectedData)
     local data = CheckDataFromKey(data)
     if not data or not CheckPerms(data.perms) then return end
@@ -8,24 +9,22 @@ RegisterNetEvent('ps-adminmenu:server:FreezePlayer', function(data, selectedData
     local target = selectedData["Player"].value
 
     local ped = GetPlayerPed(target)
-    local Player = QBCore.Functions.GetPlayer(target)
+    local Player = ESX.GetPlayerFromId(target)
 
     if not frozen then
         frozen = true
         FreezeEntityPosition(ped, true)
-        QBCore.Functions.Notify(src,
-            locale("Frozen",
-                Player.PlayerData.charinfo.firstname ..
-                " " .. Player.PlayerData.charinfo.lastname .. " | " .. Player.PlayerData.citizenid), 'Success', 7500)
+        TriggerClientEvent('esx:showNotification', src,
+            _U("Frozen",
+                Player.getName()), 'Success', 7500)
     else
         frozen = false
         FreezeEntityPosition(ped, false)
-        QBCore.Functions.Notify(src,
-            locale("deFrozen",
-                Player.PlayerData.charinfo.firstname ..
-                " " .. Player.PlayerData.charinfo.lastname .. " | " .. Player.PlayerData.citizenid), 'Success', 7500)
+        TriggerClientEvent('esx:showNotification', src,
+            _U("deFrozen",
+                Player.getName()), 'Success', 7500)
     end
-    if Player == nil then return QBCore.Functions.Notify(src, locale("not_online"), 'error', 7500) end
+    if Player == nil then return TriggerClientEvent('esx:showNotification', src, _U("not_online"), 'error', 7500) end
 end)
 
 -- Drunk Player
@@ -35,16 +34,52 @@ RegisterNetEvent('ps-adminmenu:server:DrunkPlayer', function(data, selectedData)
 
     local src = source
     local target = selectedData["Player"].value
-    local targetPed = GetPlayerPed(target)
-    local Player = QBCore.Functions.GetPlayer(target)
+    local Player = ESX.GetPlayerFromId(target)
 
     if not Player then
-        return QBCore.Functions.Notify(src, locale("not_online"), 'error', 7500)
+        return TriggerClientEvent('esx:showNotification', src, _U("not_online"), 'error', 7500)
     end
 
     TriggerClientEvent('ps-adminmenu:client:InitiateDrunkEffect', target)
-    QBCore.Functions.Notify(src,
-        locale("playerdrunk",
-            Player.PlayerData.charinfo.firstname ..
-            " " .. Player.PlayerData.charinfo.lastname .. " | " .. Player.PlayerData.citizenid), 'Success', 7500)
+    TriggerClientEvent('esx:showNotification', src,
+        _U("playerdrunk",
+            Player.getName()), 'Success', 7500)
+end)
+
+-- Set on fire
+RegisterNetEvent('ps-adminmenu:server:SetOnFire', function(data, selectedData)
+    local data = CheckDataFromKey(data)
+    if not data or not CheckPerms(data.perms) then return end
+
+    local src = source
+    local target = selectedData["Player"].value
+    local Player = ESX.GetPlayerFromId(target)
+
+    if not Player then
+        return TriggerClientEvent('esx:showNotification', src, _U("not_online"), 'error', 7500)
+    end
+
+    TriggerClientEvent('ps-adminmenu:client:SetOnFire', target)
+    TriggerClientEvent('esx:showNotification', src,
+        "Player on fire",
+            Player.getName(), 'Success', 7500)
+end)
+
+-- Explode Player
+RegisterNetEvent('ps-adminmenu:server:ExplodePlayer', function(data, selectedData)
+    local data = CheckDataFromKey(data)
+    if not data or not CheckPerms(data.perms) then return end
+
+    local src = source
+    local target = selectedData["Player"].value
+    local Player = ESX.GetPlayerFromId(target)
+
+    if not Player then
+        return TriggerClientEvent('esx:showNotification', src, _U("not_online"), 'error', 7500)
+    end
+
+    TriggerClientEvent('ps-adminmenu:client:ExplodePlayer', target)
+    TriggerClientEvent('esx:showNotification', src,
+        "Player Exploded",
+            Player.getName(), 'Success', 7500)
 end)
