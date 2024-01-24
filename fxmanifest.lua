2f2c4cfdaab7ae78a628f8a69db122d7630ca43b
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2024-01-24 11:35:00 -0500

    1.1.4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'cerulean'
 game "gta5"
 
 author "Project Sloth & OK1ez"
-version '1.1.3'
+version '1.1.4'
 description 'Admin Menu'
 repository 'https://github.com/Project-Sloth/ps-adminmenu'
 
