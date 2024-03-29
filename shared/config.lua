de93a74cb00979858c6f8b5970404867b5addb1d
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2024-03-29 03:17:15 -0400

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,6 +1,6 @@
 Config = Config or {}
 
-Config.Fuel = "ps-fuel"        -- "ps-fuel", "LegacyFuel", "rhd_fuel", "cdn-fuel", "ox_fuel"
+Config.Fuel = "ps-fuel"        -- "ps-fuel", "LegacyFuel", "ox_fuel"
 Config.ResourcePerms = 'admin' -- permission to control resource(start stop restart)
 Config.ShowCommandsPerms = 'admin' -- permission to show all commands
 Config.RenewedPhone = false    -- if you use qb-phone from renewed. (multijob)
